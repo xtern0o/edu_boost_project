ea33e82d1c77c77d39875f475501c9d708e97716
xtern0o <maks060109@ya.ru>
2023-04-06 19:29:22 +0300

    register form + html

---
+++ forms/register_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField, StringField, RadioField
+from wtforms.validators import DataRequired, Length
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField("Электронная почта", validators=[DataRequired()])
+    first_name = StringField("Имя", validators=[DataRequired(), Length(min=2)])
+    second_name = StringField("Фамилия", validators=[DataRequired(), Length(min=2)])
+    invite_code = StringField("Пригласительный код")
+    user_type_teacher = RadioField("Учитель")
+    user_type_student = RadioField("Ученик")
