195441834e8c1d3285df9c6a0ed298ac523aa227
xtern0o <maks060109@ya.ru>
2023-04-07 19:46:43 +0300

    small refactor

--- forms/chat_form.py
+++ forms/chat_form.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
+from wtforms import SubmitField, TextAreaField, StringField
 from wtforms.validators import DataRequired, Length
 from flask import url_for
 
 
 class ChatForm(FlaskForm):
-    message = TextAreaField()
+    message = StringField("Сообщение")
+    submit = SubmitField()
